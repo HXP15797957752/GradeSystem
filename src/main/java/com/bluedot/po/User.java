1a834f6e4acaa963053d829e0cec0b32f3bb143c
zhangguozhangyue <1181601469@qq.com>
2018-11-24 14:33:51 +0800

    登入

---
+++ src/main/java/com/bluedot/po/User.java
@@ -0,0 +1,67 @@
+package com.bluedot.po;
+
+
+public class User {
+    private Integer id;
+
+    private String userId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+    
+    private String phone;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+  
+}
